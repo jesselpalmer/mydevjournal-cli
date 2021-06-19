e95ddd96ab8283be2de88075256c9bf25a44f334
Jesse Palmer <jesselpalmer@gmail.com>
2021-06-19 16:51:44 -0700

    chore: clean up comments

--- index.ts
+++ index.ts
@@ -1,15 +1,31 @@
-// journal if there are no parameters creates a new folder 
+/*
+  journal command
 
-/* 
-* journal command
+  no params
 
   checks to see if there is a template setup.
-  do you want to setup a new template?
-  would you like to create the template yours                elf or would you prefer a 
-   prefilled tempa
-  no param
-  creates a new folder in current year and month. 
+  
+  if there is no template setup
+
+    ask user if they want to setup a new template
+
+      if the user selects yes ask if the user would like to use a prefilled 
+      template or would they like to create a new template.
+
+        if the user does wants to use a prefilled template, copy the sample
+        display the sample template file, and then ask the user if they want 
+        any updates.
+
+    if the user doesnt want to setup a template save the answer in their config
+    file
+  
+  if there is a template setup
+    create a new directory using the current year and month. 
 
   has param
-// In case we want to use the md
+ In case we want to use the md
+ */
 
+const journal = () => {
+  console.log(process.argv);
+}
