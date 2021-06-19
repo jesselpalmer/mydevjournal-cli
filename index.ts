213b7c06c9ad5147a5bc34bb0aa9b74108b5d0b5
Jesse Palmer <jesselpalmer@gmail.com>
2021-06-19 16:16:00 -0700

    chore: add files

---
+++ index.ts
@@ -0,0 +1,15 @@
+// journal if there are no parameters creates a new folder 
+
+/* 
+* journal command
+
+  checks to see if there is a template setup.
+  do you want to setup a new template?
+  would you like to create the template yours                elf or would you prefer a 
+   prefilled tempa
+  no param
+  creates a new folder in current year and month. 
+
+  has param
+// In case we want to use the md
+
